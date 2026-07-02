75917a3f4e81c40fff4d8e93d0df07a85f574189
Ifeoluwa Andrew Omole <iomole3@gmail.com>
2026-07-02 21:56:41 +0100

    Add fingerprint box and styles

---
+++ ui/app/common/biometrics/directives/fingerprintBox.js
@@ -0,0 +1,55 @@
+'use strict';
+
+angular.module("bahmni.common.biometrics")
+    .directive("fingerprintBox", function () {
+        var link = function ($scope) {
+            $scope.getImgSrc = function () {
+                if ($scope.error) {
+                    return '../images/fp-error.png';
+                }
+                if ($scope.imgSrc) {
+                    return $scope.imgSrc;
+                }
+                if ($scope.success) {
+                    return '../images/fp-success.png';
+                }
+                return '../images/fp-default.png';
+            };
+
+            $scope.overlaySrc = '../images/fp-scanning.png';
+        };
+
+        return {
+            restrict: 'E',
+            link: link,
+            scope: {
+                scanning: '=',
+                error: '=',
+                imgSrc: '=',
+                success: '=',
+                canEdit: '=',
+                fingerprintCount: '='
+            },
+            template:
+                "<div>" +
+                "  <img class=\"fp-overlay\"ng-hide=\"!scanning\"ng-src= \"{{ overlaySrc }}\"/>" +
+                "  <img ng-src=\"{{ getImgSrc() }}\" " +
+                "       ng-class=\"{" +
+                "         'fp-default': scanning," +
+                "         'fp-error': error," +
+                "         'fp-img': imgSrc," +
+                "         'fp-success': success && !imgSrc" +
+                "       }\" />" +
+                "  <span ng-hide=\"scanning || imgSrc\" " +
+                "        ng-class=\"{" +
+                "          'fp-error-text': error," +
+                "          'fp-success-text': success && !imgSrc" +
+                "        }\">" +
+                "    <span ng-if=\"error\">{{ error }}</span>" +
+                "    <span ng-if=\"!error && success && fingerprintCount != null\">" +
+                "      {{ fingerprintCount }} fingerprints enrolled" +
+                "    </span>" +
+                "  </span>" +
+                "</div>"
+        };
+    });
